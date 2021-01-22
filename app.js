92ce99a8cce057320ecc74b769c927863bf71a1b
on1ystar <tjdwls0607@naver.com>
2021-01-23 04:54:09 +0900

    routing

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { userRouter } from "./router";
+
+const app = express();
+
+function handleHome(req, res) {
+  res.send("Welcome to my home");
+}
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+
+app.use("/user", userRouter);
+export default app;
