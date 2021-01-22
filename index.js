56a9dd462d6ca2d823706cce99db556488a700c0
on1ystar <tjdwls0607@naver.com>
2021-01-23 04:40:19 +0900

    middlewares

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const express = require("express");
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
-PORT = 4000;
+const PORT = 4000;
 
 function handleListening() {
   console.log("Listening on: http://localhost:4000");
 }
+
+function handleHome(req, res) {
+  res.send("Welcome to my home");
+}
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmetI());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+
 app.listen(PORT, handleListening);
