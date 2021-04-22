0c6a0dde3b3814cd53a138ebc28fe90d9fba4c90
on1ystar <tjdwls0607@naver.com>
2021-04-22 15:07:13 +0900

    rebase

--- init.js
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-
-const PORT = 4000;
-
-function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
-
-app.listen(PORT, handleListening);
--- routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { changePassword, editProfile, userDetail } from "../controllers/userController";
-import routes from "../routes";
-
-export const userRouter = express.Router();
-
-userRouter.get(routes.userDetail, userDetail);
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
