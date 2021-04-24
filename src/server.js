70c6c2180d99171a43a2361d583f360ff021e2b1
on1ystar <tjdwls0607@naver.com>
2021-04-24 20:10:58 +0900

    setup middleware

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
+/* eslint-disable prefer-destructuring */
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan('dev');
 
-app.get('/', (req, res) => res.send('This is the first home for Express.'));
+const handleHome = (req, res) =>
+  res.send('This is the first home for Express.');
+
+app.use(logger);
+app.get('/', handleHome);
 
 app.listen(PORT, () =>
   console.log(`✅ Server listening on http://127.0.0.1:${PORT}/`)
