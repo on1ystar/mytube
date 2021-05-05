049161bd11e58c5a7ed2a5007d302e12bea81564
on1ystar <tjdwls0607@naver.com>
2021-05-05 20:06:23 +0900

    dividing templates by using include and inheritance keywords

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,9 @@
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 
-export const trending = (req, res) => res.render('home');
+export const trending = (req, res) => res.render('home', { pageTitle: 'home' });
 export const search = (req, res) => res.sned('Search ');
 export const upload = (req, res) => res.send('Upload Video');
-export const see = (req, res) => {
-  console.warn(req.params);
-  res.send('See video !');
-};
+export const see = (req, res) => res.render('watch');
 export const edit = (req, res) => {
   console.warn(req.params);
   res.send('Edit video !');
