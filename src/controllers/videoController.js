e4825bd879ac3749d9a47816b6b6ebc8357ca18e
on1ystar <tjdwls0607@naver.com>
2021-05-07 19:05:17 +0900

    pug mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,34 @@
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 
-export const trending = (req, res) => res.render('home', { pageTitle: 'home' });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: 'First video',
+      rating: 5,
+      comments: 4,
+      createdAt: '2 minutes ago',
+      views: 100,
+      id: 1
+    },
+    {
+      title: 'Second video',
+      rating: 5,
+      comments: 4,
+      createdAt: '2 minutes ago',
+      views: 100,
+      id: 2
+    },
+    {
+      title: 'Third video',
+      rating: 5,
+      comments: 4,
+      createdAt: '2 minutes ago',
+      views: 100,
+      id: 3
+    }
+  ];
+  res.render('home', { pageTitle: 'home', videos });
+};
 export const search = (req, res) => res.sned('Search ');
 export const upload = (req, res) => res.send('Upload Video');
 export const see = (req, res) => res.render('watch');
