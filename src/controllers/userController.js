05c762511f66333025c1737ebe27c92d5964e2dd
on1ystar <tjdwls0607@naver.com>
2021-05-29 23:33:10 +0900

    log in and check username with password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,13 @@
+import bcrypt from 'bcrypt';
 import User from '../models/User';
 
 export const getJoin = (req, res) => {
-  res.render('join', { pagetitle: 'Join' });
+  res.render('join', { pageTitle: 'Join' });
 };
 export const postJoin = async (req, res) => {
   const { username, email, password, password2, name, location } = req.body;
   const pageTitle = 'join';
-  const exists = User.exists({ $or: [{ username }, { email }] });
+  const exists = await User.exists({ $or: [{ username }, { email }] });
   if (password !== password2) {
     return res.status(400).render('join', {
       pageTitle,
@@ -37,19 +38,25 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req, res) =>
   res.render('login', { pageTitle: 'Login' });
 
-export const postLogin = (req, res) => {
+export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const exists = User.exists({ username });
+  const user = await User.findOne({ username });
   const pageTitle = 'Login';
-  if (!exists) {
+  if (!user) {
     return res.status(400).render('/login', {
       pageTitle,
       errorMessage: 'An account with this username does not exists'
     });
   }
-  return res.end();
+  const matchPassword = await bcrypt.compare(password, user.password);
+  if (!matchPassword) {
+    return res.status(400).render('/login', {
+      pageTitle,
+      errorMessage: 'Invalid password'
+    });
+  }
+  return res.redirect('/');
 };
-
 export const edit = (req, res) => res.send('Edit user');
 export const remove = (req, res) => res.send('Remove user');
 export const logout = (req, res) => res.send('Logout');
